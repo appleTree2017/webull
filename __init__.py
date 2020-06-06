5671e521c80720e9cf1be8ad6b2e3476ef7d7de1
appleTree2017 <172864382@qq.com>
2020-06-06 00:50:15 -0400

    __init__.py
    This edit enable to call the get_mfa( ) method, makes webull.get_mfa( ) work. In the previous version, if run webull.get_mfa( ) , an error message "webull does not have attribute get_mfa( )" showed up. So I made this change to solve the problem.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from webull import webull
+
+ """please note """
+# with this change, when you want to call get_mfa( ), use the following codes:
+# from webull import webull# for paper trading, import 'paper_webull'
+# wb=webull.webull()
+# wb.get_mfa("your_email")
